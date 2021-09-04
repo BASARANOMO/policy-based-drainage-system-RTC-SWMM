d9047a5eb2c124bf71af854b9eeb3d892c61cf10
BASARANOMO <basaranomo@outlook.com>
2021-09-04 23:07:07 +0800

    add results

--- src/scripts/run_scripts.py
+++ src/scripts/run_scripts.py
@@ -1,5 +1,5 @@
 import os
 
 for rain_event in range(1, 16):
-    cmd = f"python test_trained_model.py {rain_event} episodes_3000 {True}"
+    cmd = f"python test_trained_model.py {rain_event} reward_244_episodes_3000 {True}"
     os.system(cmd)
